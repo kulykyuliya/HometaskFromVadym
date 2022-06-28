c151b20f46455451f354a9fbb2bd5ab58526e048
Nestors <nestors@Yuliya-Kulyk-MacBook-Pro.local>
2022-06-28 19:29:41 +0300

    MathTests

---
+++ src/test/java/JavaCore/MinusMethodsAndTests/MinusMethodsInt.java
@@ -0,0 +1,7 @@
+package JavaCore.MinusMethodsAndTests;
+
+public class MinusMethodsInt {
+    public int minus (int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/test/java/JavaCore/PlusMethodsAndTests/PlusMethodsString.java
@@ -0,0 +1,7 @@
+package JavaCore.PlusMethodsAndTests;
+
+public class PlusMethodsString {
+    public String plusString(String a, String b) {
+        return a + b;
+    }
+}
